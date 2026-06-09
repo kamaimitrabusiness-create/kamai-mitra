c128cee7af57bf500aef7d889e091d5caa8d3a40
KamaiMitraUser <kamaimitrabusiness@gmail.com>
2026-06-09 11:54:35 +0530

    Fix missing bracket in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    // id("com.google.gms.google-services") // Yeh line humne comment out kar di hai taaki error na aaye
 }
 
 android {
@@ -38,3 +37,4 @@ kotlin {
 
 flutter {
     source = "../.."
+}
