5ba1793aa4d51858871d38a4bd5b1ee366d2631a
KamaiMitraUser <kamaimitrabusiness@gmail.com>
2026-06-09 11:45:34 +0530

    Remove google-services plugin to fix build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") // Yeh nayi line humne add kar di hai
+    // id("com.google.gms.google-services") // Yeh line humne comment out kar di hai taaki error na aaye
 }
 
 android {
@@ -16,10 +16,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.kamai_mitra"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -28,8 +25,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -43,4 +38,3 @@ kotlin {
 
 flutter {
     source = "../.."
-}
